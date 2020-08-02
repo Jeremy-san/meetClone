6f6d3523b648c389a8217eba6907ce1fe98910aa
Jeremy-san <shehata.jeremy@gmail.com>
2020-08-02 06:07:52 +0200

    initial work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ArticleComponent } from './article/article.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent,
+    HomeComponent,
+    ArticleComponent,
+    MarketplaceComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
