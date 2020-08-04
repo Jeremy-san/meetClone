0a14dac88a417161d9d0d5226ecf0dd244fad452
Jeremy-san <shehata.jeremy@gmail.com>
2020-08-04 06:08:33 +0200

    router link check

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ArticleComponent } from './article/article.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { ContactComponent } from './contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'gallery', component: GalleryComponent
+  },
+  {
+    path: 'article', component: ArticleComponent
+  },
+  {
+    path: 'shop', component: MarketplaceComponent
+  },
+  {
+    path: 'contact', component: ContactComponent
+  },
+  {
+    path: '**', redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+//  sliderImages: any = document.querySelectorAll('.slide');
+//  arrowLeft = document.querySelector('#arrow-left');
+//  arrowRight = document.querySelector('#arrow-right');
+//  current = 0;
+
+  constructor(private myElement: ElementRef) { }
 
   ngOnInit(): void {
   }
 
+// // Clear all images
+//  reset() {
+//   // tslint:disable-next-line: prefer-for-of
+//   for (let i = 0; i < this.sliderImages.length; i++) {
+//       this.sliderImages [i].style.display = 'none';
+//   }
+// }
+
+// // Init slider
+
+//  startSlider() {
+//   reset();
+//   this.sliderImages[0].style.display = 'block';
+// }
+
+// // Show previous
+//  slideLeft() {
+//   reset();
+//   this.sliderImages[this.current - 1].style.display = 'block';
+//   this.current--;
+// }
+
+// // Show next
+//  slideRight() {
+//   reset();
+//   this.sliderImages[this.current + 1].style.display = 'block';
+//   this.current++;
+// }
+
+
+// // left arrow click
+// this.arrowLeft.addEventListener('click', function () {
+//   if (this.current === 0) {
+//       this.current = this.sliderImages.length;
+//   }
+//   slideLeft();
+// });
+
+
+
+// // right arrow click
+// arrowRight.addEventListener('click', function() {
+//   if (this.current === this.sliderImages.length - 1) {
+//       this.current = -1;
+//   }
+//   slideRight();
+// });
+
+// startSlider();
+
 }
+
